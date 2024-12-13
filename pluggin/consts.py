7ad5985b815388a895e60cfe38d93e8efcdb5322
Ch3did <kphch3did@gmail.com>
2024-12-13 18:17:43 -0300

    fix: solve images path

--- pluggin/consts.py
+++ pluggin/consts.py
@@ -8,7 +8,7 @@ class AZAZEL_STONE:
     DEAFULT_LANGUAGE = "pt-BR"
     FOLDER = Path().cwd()
     TEMP = FOLDER / "temp"
-    IMAGES = FOLDER / "pluggin" / "images"
+    IMAGES = FOLDER / "images"
     ACTIVATION_PHRASE = "Azazel"
     FAIL_TO_UNDERSTAND = "Não consegui entender o que você disse"
     LLM_OPTIONS = ["Ollama Server", "GPT-4"]
