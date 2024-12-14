a98ead15e34103b21fcab315c86a1630f592e559
Ch3did <kphch3did@gmail.com>
2024-12-14 15:31:45 -0300

    fix: rollback change_llm

--- pluggin/libraries/server.py
+++ pluggin/libraries/server.py
@@ -27,12 +27,12 @@ class Server:
         headers = {"Content-Type": "application/json"}
         payload = {"ConfigName": "llm_model", "Value": f"{llm_option}"}
 
-        # response = requests.patch(url, headers=headers, json=payload)
+        response = requests.patch(url, headers=headers, json=payload)
 
-        # if response.status_code == 200:
-        #     print("Resposta:", response.json())
-        # else:
-        #     print(f"Erro {response.status_code}: {response.text}")
+        if response.status_code == 200:
+            print("Resposta:", response.json())
+        else:
+            print(f"Erro {response.status_code}: {response.text}")
 
     def get_info(self):
         url = f"{self.base_url}/info"
