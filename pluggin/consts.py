385f4ad35626465f0a4e25d3401ef3d088bbad42
Ch3did <kphch3did@gmail.com>
2025-01-09 10:03:03 -0300

    fix: removed plugin path from consts

--- pluggin/consts.py
+++ pluggin/consts.py
@@ -6,10 +6,10 @@ class AZAZEL_STONE:
     DEAFULT_LANGUAGE = "pt-BR"
     FOLDER = Path().cwd()
     TEMP = "/var/tmp"
+    HISTORY_PATH = Path(f"{TEMP}/historic.txt")
     USE_VOICE = os.environ.get("USE_VOICE")
-    PLUGGIN_PATH = FOLDER / "pluggin"
     OUTPUT_FILE = Path(f"{TEMP}/output.wav")
-    IMAGES = PLUGGIN_PATH / "images"
+    IMAGES = FOLDER / "images"
     ACTIVATION_PHRASE = "Azazel"
     LLM_OPTIONS = ["Ollama Server", "GPT-4"]
     EGO = """ *** Answer as briefly as possible, without writing titles 
